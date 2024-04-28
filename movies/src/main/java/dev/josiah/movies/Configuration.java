5a1e14730fb65b1dff7c4441359852746728c91f
Josiah Tsang <jtsang02@student.ubc.ca>
2024-04-28 03:28:59 -0700

    Create Configuration.java

---
+++ movies/src/main/java/dev/josiah/movies/Configuration.java
@@ -0,0 +1,19 @@
+package dev.josiah.movies;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@org.springframework.context.annotation.Configuration
+@EnableWebMvc
+public class Configuration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
